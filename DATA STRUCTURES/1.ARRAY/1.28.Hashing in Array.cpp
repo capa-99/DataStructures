14df9ed282c0710340f1898eb5826691a2a13d69
capa-99 <tasic.sara5@gmail.com>
2024-12-23 18:09:41 +0100

    Finishing up on arrays and starting on matrices

---
+++ DATA STRUCTURES/1.ARRAY/1.28.Hashing in Array.cpp
@@ -0,0 +1,4 @@
+//Hashing is a technique used in data structures that efficiently stores and retrieves data in a way that allows for quick access.
+//Hashing involves mapping data to a specific index in a hash table (an array of items) using a hash function that enables fast retrieval of information based on its key.
+//All three operations (search, insert and delete) can be achieved in O(1) time on average.
+//Hashing is mainly used to implement a set of distinct items and dictionaries (key value pairs).
